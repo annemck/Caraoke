cf7240a1d0a44af246495c328dabad4784e66870
annemck <annekh88@gmail.com>
2019-03-29 11:27:08 +0000

    song class created

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -5,6 +5,16 @@ require_relative('../song.rb')
 
 class TestSong < MiniTest::Test
   
+  def setup
+    @song = Song.new("Last Friday Night", "Katy Perry")
+  end
   
+  def test_song_has_title
+    assert_equal("Last Friday Night", @song.title)
+  end
+  
+  def test_song_has_artist
+    assert_equal("Katy Perry", @song.artist)
+  end
   
 end
