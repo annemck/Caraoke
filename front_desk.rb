38fb9127fae527e4cb707f1b983888b2ecd70242
annemck <annekh88@gmail.com>
2019-03-30 13:12:57 +0000

    front_desk class added

---
+++ front_desk.rb
@@ -0,0 +1,5 @@
+class FrontDesk
+  
+  
+  
+end
---
+++ specs/front_desk_spec.rb
@@ -0,0 +1,10 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../front_desk.rb')
+
+
+class TestFrontDesk < MiniTest::Test
+  
+  
+  
+end
